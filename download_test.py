91daa5166e2010d22a396e1ab961bf1f979583e1
marynam99 <66097819+marynam99@users.noreply.github.com>
2021-03-28 15:46:26 +0900

    Add files via upload

---
+++ download_test.py
@@ -0,0 +1,26 @@
+# import requests
+#
+# url = 'http://google.com/favicon.ico'
+# r = requests.get(url, allow_redirects=True)
+# open('google.ico', 'wb').write(r.content)
+
+
+import requests
+import re
+
+def get_filename_from_cd(cd):
+    """
+    Get filename from content-disposition
+    """
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+
+
+url = 'http://google.com/favicon.ico'
+r = requests.get(url, allow_redirects=True)
+filename = get_filename_from_cd(r.headers.get('content-disposition'))
+open(filename, 'wb').write(r.content)
